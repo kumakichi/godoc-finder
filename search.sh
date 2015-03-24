d8ff8412e3cdf02725f5669b6c866e1eb55f369d
cm_san <xbapj1@gmail.com>
2015-03-24 14:12:17 +0800

    fixed output filter and script path detect

--- search.sh
+++ search.sh
@@ -2,7 +2,12 @@
 
 DB_FILE=PKG.db
 
-realscript=$(readlink ${BASH_SOURCE})
+realscript=$(readlink ${BASH_SOURCE}) # soft link
+if [ "$realscript" = "" ]
+then
+    realscript=$(ls -l ${BASH_SOURCE} | awk '{print $9}') # called : ./search.sh
+fi
+
 prog_path=${realscript%/*}
 DB=${prog_path}/${DB_FILE}
 
@@ -23,5 +28,5 @@ name=$1
 if [ $# -eq 1 ]
 then
     echo -e "funcs and types of '$name' :\n"
-    grep -i -E [A-Za-z]*$name.* $DB | awk '{print $1,$3}'
+    grep -i -E ^[A-Za-z]*$name.* $DB | awk '{print $1,$3}'
 fi
