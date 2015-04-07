a93ab10f64ba0d522ba0bfad12c311d24e3dddf3
san <xyesan@gmail.com>
2015-04-07 20:11:11 +0800

    filter the same line

--- build.sh
+++ build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 DB=PKG.db
+TMPDB=tmp.PKG.db
 
 function funs()
 {
     pkg=$1
-    godoc $pkg | awk -v p=${pkg} -F'[ (]' '/^func /{if(length($2)>1)print $2,"=func=",p}' >>$DB
+    godoc $pkg | awk -v p=${pkg} -F'[ (]' '/^func /{if(length($2)>1)print $2,"=func=",p}' >>$TMPDB
 }
 
 function methods()
 {
     pkg=$1
-    godoc $pkg | awk -F ')' '/^func \(/{if(NF==3)print $2}' | awk -v p=${pkg} -F'[ (]' '{print $2,"=func=",p}' >>$DB
+    godoc $pkg | awk -F ')' '/^func \(/{if(NF==3)print $2}' | awk -v p=${pkg} -F'[ (]' '{print $2,"=func=",p}' >>$TMPDB
 }
 
 function types()
 {
     pkg=$1
-    godoc $pkg | awk -v p=${pkg} '/^type /{print $2,"=type=",p}' >>$DB
+    godoc $pkg | awk -v p=${pkg} '/^type /{print $2,"=type=",p}' >>$TMPDB
 }
 
 if [ "$GOROOT" = "" ]
@@ -26,7 +27,7 @@ then
     exit
 fi
 
->$DB
+>$TMPDB
 for each in $(find $GOROOT/src -mindepth 1 -maxdepth 10 -type d -exec echo '{}' \; | sed "s|$GOROOT/src/||g")
 do
     echo $each
@@ -34,3 +35,6 @@ do
     types $each
     methods $each
 done
+
+awk '!a[$0]++' $TMPDB > $DB
+rm $TMPDB
