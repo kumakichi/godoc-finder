246ef671edf688a48d06cd103cb6d7d326582e65
cm_san <xbapj1@gmail.com>
2015-07-10 18:37:34 +0800

    type filter

--- search.sh
+++ search.sh
@@ -2,6 +2,7 @@
 
 DB_FILE=PKG.db
 caseSensitive=0
+showTypes="all" #all types
 
 realscript=$(readlink ${BASH_SOURCE}) # soft link
 if [ "$realscript" = "" ]
@@ -18,12 +19,26 @@ then
     exit
 fi
 
-while getopts "s" o; do
+while getopts "st:" o; do
     case "${o}" in
         s)
             caseSensitive=1
             ;;
+        t)
+            if [ "${OPTARG}" = "f" ]
+            then
+		    showTypes="function"
+            elif [ "${OPTARG}" = "m" ]
+            then
+		    showTypes="method"
+            elif [ "${OPTARG}" = "t" ]
+            then
+		    showTypes="type"
+            fi
+            ;;
         *)
+            echo "Invalid option, prog will exit not"
+            exit
             ;;
     esac
 done
@@ -31,19 +46,31 @@ shift $((OPTIND-1))
 
 if [ $# -lt 1 ]
 then
-    echo "Usage: $0 [-s] symbolName"
-    echo "support type, func and method currently"
+    echo "Usage: $0 [-s] [-t f/t/m] symbolName"
     echo "-s if specified, symbolName case sensitive"
+    echo "-t f/t/m"
+    echo "   f -> function"
+    echo "   t -> type"
+    echo "   m -> method"
 	
     exit
 fi
 
 symbolName=$1
 
-
-if [ $caseSensitive -eq 1 ]
+if [ $caseSensitive -eq 1 ] # case sensitive
 then
-	grep -E ^[A-Za-z]*$symbolName* $DB | awk '{print $1,$3,"[",$2,"]:: godoc "$3,$1}'
+	if [ $showTypes != "all" ]
+	then
+		grep -E ^[A-Za-z]*$symbolName* $DB | awk -v types=$showTypes '{if($2==types)print $1,$3,":: godoc "$3,$1}'
+	else
+		grep -E ^[A-Za-z]*$symbolName* $DB | awk '{print $1,$3,"[",$2,"]:: godoc "$3,$1}'
+	fi
 else
-	grep -i -E ^[A-Za-z]*$symbolName* $DB | awk '{print $1,$3,"[",$2,"]:: godoc "$3,$1}'
+	if [ $showTypes != "all" ]
+	then
+		grep -i -E ^[A-Za-z]*$symbolName* $DB | awk -v types=$showTypes '{if($2==types)print $1,$3,":: godoc "$3,$1}'
+	else
+		grep -i -E ^[A-Za-z]*$symbolName* $DB | awk '{print $1,$3,"[",$2,"]:: godoc "$3,$1}'
+	fi
 fi
