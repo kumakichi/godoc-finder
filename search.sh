89b8cac69b06f22b0fa1a5890986772633312a08
kumakichi <xyesan@gmail.com>
2019-05-13 16:07:29 +0800

    if only 1 item found,show it

--- search.sh
+++ search.sh
@@ -6,85 +6,82 @@ showTypes="all" #all types
 pkgName=""
 
 realscript=$(readlink ${BASH_SOURCE}) # soft link
-if [ "$realscript" = "" ]
-then
-    realscript=$(ls -l ${BASH_SOURCE} | awk '{print $9}') # called : ./search.sh
+if [ "$realscript" = "" ]; then
+	realscript=$(ls -l ${BASH_SOURCE} | awk '{print $9}') # called : ./search.sh
 fi
 
 prog_path=${realscript%/*}
 DB=${prog_path}/${DB_FILE}
 
-if [ ! -f $DB ]
-then
-    echo "db file '$DB' not found, run 'build.sh' first"
-    exit
+if [ ! -f $DB ]; then
+	echo "db file '$DB' not found, run 'build.sh' first"
+	exit
 fi
 
 while getopts "st:p:" o; do
-    case "${o}" in
-        s)
-            caseSensitive=1
-            ;;
-        t)
-            if [ "${OPTARG}" = "f" ]
-            then
-		    showTypes="function"
-            elif [ "${OPTARG}" = "m" ]
-            then
-		    showTypes="method"
-            elif [ "${OPTARG}" = "t" ]
-            then
-		    showTypes="type"
-            fi
-            ;;
-        p)
-            pkgName=${OPTARG}
-            ;;
-        *)
-            echo "Invalid option, prog will exit not"
-            exit
-            ;;
-    esac
+	case "${o}" in
+	s)
+		caseSensitive=1
+		;;
+	t)
+		if [ "${OPTARG}" = "f" ]; then
+			showTypes="function"
+		elif [ "${OPTARG}" = "m" ]; then
+			showTypes="method"
+		elif [ "${OPTARG}" = "t" ]; then
+			showTypes="type"
+		fi
+		;;
+	p)
+		pkgName=${OPTARG}
+		;;
+	*)
+		echo "Invalid option, prog will exit not"
+		exit
+		;;
+	esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
-if [ $# -lt 1 ]
-then
-    echo "Usage: $0 [-s] [-t f/t/m] [-p pkgName] symbolName"
-    echo "-s if specified, symbolName case sensitive"
-    echo "-p search only the specified package name"
-    echo "-t f/t/m"
-    echo "   f -> function"
-    echo "   t -> type"
-    echo "   m -> method"
-	
-    exit
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 [-s] [-t f/t/m] [-p pkgName] symbolName"
+	echo "-s if specified, symbolName case sensitive"
+	echo "-p search only the specified package name"
+	echo "-t f/t/m"
+	echo "   f -> function"
+	echo "   t -> type"
+	echo "   m -> method"
+
+	exit
 fi
 
 symbolName=$1
 resultContent=""
 
-if [ $caseSensitive -eq 1 ] # case sensitive
-then
-	if [ $showTypes != "all" ]
-	then
-		resultContent=$(grep -E ^[A-Za-z]*$symbolName $DB | awk -v types=$showTypes '{if($2==types)print $1,$3,":: godoc "$3,$1";"}')
+if [ $caseSensitive -eq 1 ]; then # case sensitive
+	if [ $showTypes != "all" ]; then
+		resultContent=$(grep -E ^[A-Za-z]*$symbolName $DB | awk -v types=$showTypes '{if($2==types)print $1,$3,":: go doc "$3,$1";"}')
 	else
-		resultContent=$(grep -E ^[A-Za-z]*$symbolName $DB | awk '{print $1,$3,"[",$2,"]:: godoc "$3,$1";"}')
+		resultContent=$(grep -E ^[A-Za-z]*$symbolName $DB | awk '{print $1,$3,"[",$2,"]:: go doc "$3,$1";"}')
 	fi
 else
-	if [ $showTypes != "all" ]
-	then
-		resultContent=$(grep -i -E ^[A-Za-z]*$symbolName $DB | awk -v types=$showTypes '{if($2==types)print $1,$3,":: godoc "$3,$1";"}')
+	if [ $showTypes != "all" ]; then
+		resultContent=$(grep -i -E ^[A-Za-z]*$symbolName $DB | awk -v types=$showTypes '{if($2==types)print $1,$3,":: go doc "$3,$1";"}')
 	else
-		resultContent=$(grep -i -E ^[A-Za-z]*$symbolName $DB | awk '{print $1,$3,"[",$2,"]:: godoc "$3,$1";"}')
+		resultContent=$(grep -i -E ^[A-Za-z]*$symbolName $DB | awk '{print $1,$3,"[",$2,"]:: go doc "$3,$1";"}')
 	fi
 fi
 
-if [ "$pkgName" != "" ]
-then
-	#echo $resultContent | sed -e 's/; /\n/g;s/;/\n/g' | awk -v p=$pkgName '{if($2==p)print $0}' | sed 's/; /\n/g'
+if [ "$pkgName" != "" ]; then
+	resultContent=$(echo $resultContent | sed 's@;$@@g')
+	itemCount=$(echo $resultContent | sed 's/; /\n/g' | awk -v p=$pkgName '$2~p{print $0}' | sed 's/; /\n/g' | wc -l)
 	echo $resultContent | sed 's/; /\n/g' | awk -v p=$pkgName '$2~p{print $0}' | sed 's/; /\n/g'
 else
+	resultContent=$(echo $resultContent | sed 's@;$@@g')
+	itemCount=$(echo $resultContent | sed -e 's/; /\n/g;s/;/\n/g' | wc -l)
 	echo $resultContent | sed -e 's/; /\n/g;s/;/\n/g'
+fi
+
+if [ $itemCount -eq 1 ]; then
+	eval $(echo $resultContent | awk -F'::' '{print $2}')
 fi
